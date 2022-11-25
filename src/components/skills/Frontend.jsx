d6b74c9a86cd500cae444b3b8efaabdc8be58687
leikezej <jezedevkiel21@gmail.com>
2022-11-25 18:41:48 +0800

    Fix Skills Area

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -6,30 +6,8 @@ const Frontend = () => {
     <div className="skills__content">
       <h3 className="skills__title">Frontend</h3>
       
-      
       <div className="skills__box">
           <div className="skills__group">
-            <div className="skills__data">
-              <i className="bx bx-badge-check"></i>
-            
-                <h3 className="skills__name">JavaScript</h3>
-                <span className="skills__level">Master</span>
-            </div>
-            
-            <div className="skills__data">
-              <i className="bx bx-badge-check"></i>
-            
-                <h3 className="skills__name">JavaScript</h3>
-                <span className="skills__level">Master</span>
-            </div>
-            
-            <div className="skills__data">
-              <i className="bx bx-badge-check"></i>
-            
-                <h3 className="skills__name">JavaScript</h3>
-                <span className="skills__level">Master</span>
-            </div>
-            
             <div className="skills__data">
               <i className="bx bx-badge-check"></i>
             
