32303dbc626fb5ddc62f2021e038fc7e30f7ab94
leikezej <jezedevkiel21@gmail.com>
2022-11-26 07:32:31 +0800

    FIXED Scroll-to-top Component

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./scrollup.css";
 
 const ScrollUp = () => {
   window.addEventListener("scroll", function () {
     const scrollUp = document.querySelector(".scrollUp");
-    if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-    else scrollUp.classList.remove("show-scroll");
-  })
+    if (this.scrollY >= 80) scrollUp.classList.add("scroll-header");
+    else scrollUp.classList.remove("scroll-header");
+  });
+  
+  const [Toggle, showMenu] = useState(false);
+  const [activeNave, setActiveNav] = useState("#home");
+  
   return (
-    <a href="" className="scrollup">
+    <a href="#" onClick className="scrollup">
       <i className="uil uil-arrow-up scrollup__icon"></i>
     </a>
   )
