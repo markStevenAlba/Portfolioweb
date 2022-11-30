4466dc58782d815cc483ff4b35d1c7f094de97b5
jep zekiel <jezedevkiel21@gmail.com>
2022-11-30 19:11:52 +0800

    Fix Works Area

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -3,36 +3,42 @@ import Work2 from "../../assets/work2.jpg";
 import Work3 from "../../assets/work3.jpg";
 import Work4 from "../../assets/work4.jpg";
 import Work5 from "../../assets/work5.jpg";
+import pos from "../../assets/pos.png";
+import netflix from "../../assets/netflix.png";
+import meet from "../../assets/meet.png";
+import blog from "../../assets/blog.png";
+
+
 
 export const projectsData = [
     {
       id: 1,
-      image: Work1,
+      image: pos,
       title: "POS",
       category: "Web",
       link: "/pos"
     },
     {
       id: 2,
-      image: Work2,
+      image: meet,
       title: "Meet",
       category: "App",
       link: "/meet"
     },
-    // {
-    //   id: 3,
-    //   image: Work3,
-    //   title: "Brand Design",
-    //   category: "Design",
-    //   link: "/netflix"
-    // },
-    // {
-    //   id: 4,
-    //   image: Work4,
-    //   title: "App Movies",
-    //   category: "App",
-    //   link: "/blog"
-    // },
+    {
+      id: 3,
+      image: netflix,
+      title: "Netflix Web App",
+      category: "Design",
+      link: "/netflix"
+    },
+    {
+      id: 4,
+      image: blog,
+      title: "Blog Site Web",
+      category: "Web",
+      link: "/blog"
+    },
     // {
     //   id: 5,
     //   image: Work5,
