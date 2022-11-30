88789abc5564af469288d664c91806012fba5eb1
leikezej <jezedevkiel21@gmail.com>
2022-11-30 17:16:09 +0800

    Jezekiel Data

--- src/commonData.js
+++ src/commonData.js
@@ -188,6 +188,129 @@ export const PROFILES = [
                 contents: PROJECTS
             }
         ]
+    },
+        {
+        username: "jezekielisip",
+        email: "jezedevkiel21@gmail.com",
+        profile: {
+            cover: "",
+            nickname: "jezekiel",
+            firstName: "Jezekiel",
+            lastName: "Isip",
+            position: "Backend Developer",
+            bio: "I'm creative developer based in Tacloban City, Philippines, and I'm very passionate and dedicated to my work.",
+        },    
+         contents: [
+            {
+                type: "socials",
+                title: "",
+                subtitle: "",
+                contents: [
+                    {
+                      title: "",
+                      subtitle: "",
+                      description: "",
+                      link: "https://github.com/leikezej",
+                      icon: "uil uil-github-alt",
+                    },
+                    {
+                        title: "",
+                        subtitle: "",
+                        description: "",
+                        link: "https://linkedin.com/in/john-benedict-geli-16941a21a",
+                        icon: "uil uil-linkedin",
+                      },
+                      {
+                          title: "",
+                          subtitle: "",
+                          description: "",
+                          link: "https://www.facebook.com/thebullier/",
+                          icon: "uil uil-facebook",
+                        }
+                ]
+            },
+            {
+                type: "contact",
+                title: "Get in touch",
+                subtitle: "Contact Me",
+                contents: [
+                    {
+                      title: "Email",
+                      subtitle: "jezedevkiel21@gmail.com",
+                      description: "",
+                      link: "mailto:jezedevkiel21@gmail.com",
+                      icon: "bx bx-mail-send",
+                    },
+                    {
+                        title: "Whatsapp",
+                        subtitle: "+639774461641",
+                        description: "",
+                        link: "https://www.linkedin.com/in/jezekiel-isip-1ab872215/",
+                        icon: "bx bxl-whatsapp",
+                      },{
+                        title: "Skype",
+                        subtitle: "jaybee.aicpa@outlook.com",
+                        description: "",
+                        link: "https://join.skype.com/invite/ywfxqv9NGiPP",
+                        icon: "bx bxl-skype",
+                      },
+                      {
+                          title: "Messenger",
+                          subtitle: "jezekielisip",
+                          description: "",
+                          link: "https://www.facebook.com/thebullier/",
+                          icon: "bx bxl-messenger",
+                        }
+                ]
+            },
+            {
+                type: "about",
+                title: "About Me",
+                subtitle: "My Introduction",
+                description: "Frontend developer, I create web pages with UI / UX user interface, I have years of experience and many clients are happy with the projects carried out.",
+                link: CV,
+                cover: "",
+                contents: [
+                    {
+                        title: "Experience",
+                        subtitle: "4+ Years Working",
+                        description: "",
+                        link: "",
+                        icon: "bx bx-award",
+                      },
+                      {
+                        title: "Completed",
+                        subtitle: "10+ projects",
+                        description: "",
+                        link: "",
+                        icon: "bx bx-briefcase-alt",
+                      },
+                      {
+                        title: "Support",
+                        subtitle: "Online 20/5",
+                        description: "",
+                        link: "",
+                        icon: "bx bx-support",
+                      },
+                      {
+                        title: "Accounting",
+                        subtitle: "Book keeper",
+                        description: "",
+                        link: "",
+                        icon: "bx bx-book",
+                      },
+                ]
+            },
+            {
+                type: "works",
+                title: "Portfolio",
+                subtitle: "Most recent works",
+                description: "",
+                link: "",
+                cover: "",
+                contents: PROJECTS
+            }
+        ]
     }
 ]
 
